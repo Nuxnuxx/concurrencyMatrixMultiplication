d54313aae6e6cc67b2807e295cfc2add60e1126c
ES0TERIX <thomas.carre74@gmail.com>
2023-11-16 17:39:58 +0100

    comment for javadoc + change calcul method on C22 to avoid error

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,30 @@ package org.example;
 import java.util.Scanner;
 import java.util.concurrent.ForkJoinPool;
 
+import java.util.Scanner;
+import java.util.concurrent.ForkJoinPool;
+
+/**
+ * Cette classe démontre la multiplication de matrices en utilisant le framework Fork-Join en Java.
+ */
 public class Main {
+
+    /**
+     * La méthode principale qui initialise les matrices, permet la saisie de l'utilisateur et effectue la multiplication de matrices.
+     *
+     * @param args Les arguments en ligne de commande (non utilisés dans ce programme).
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // let the user chose between manual (M) , auto (A) or random (R) matrix values
-
+        // Permet à l'utilisateur de choisir entre des valeurs de matrice manuelles (M), automatiques (A) ou aléatoires (R)
         System.out.print("Valeur 'auto' (A), 'manuel' (M) ou 'random' (R) : ");
         String value = scanner.nextLine();
 
-        System.out.print("Entrer la taille des matrices: ");
+        System.out.print("Entrer la taille des matrices : ");
         int size = scanner.nextInt();
 
+        // Vérifie si la taille est une puissance de 2
         if ((size & (size - 1)) != 0) {
             scanner.close();
             throw new IllegalArgumentException("La taille des matrices doit être une puissance de 2");
@@ -22,7 +34,7 @@ public class Main {
         int[][] A = new int[size][size]; // Initialise la matrice A
         int[][] B = new int[size][size]; // Initialise la matrice B
 
-        //based on the user choice, fill the matrix with the corresponding values
+        // En fonction du choix de l'utilisateur, remplit la matrice avec les valeurs correspondantes
         switch (value) {
             case "M":
                 enterMatrixValues(A, scanner);
@@ -40,28 +52,45 @@ public class Main {
                 scanner.close();
                 throw new IllegalArgumentException("Valeur invalide");
         }
+        printMatrix(A);
+        printMatrix(B);
 
         ForkJoinPool forkJoinPool = new ForkJoinPool();
         try {
+            // Utilise ForkJoinPool pour effectuer la multiplication de matrices en parallèle
             MatrixMultiplier task = new MatrixMultiplier(A, B, size);
             int[][] result = forkJoinPool.invoke(task);
 
             System.out.println("Matrice Résultat:");
             printMatrix(result);
         } finally {
+            // Ferme ForkJoinPool
             forkJoinPool.shutdown();
         }
     }
+
+    /**
+     * Permet à l'utilisateur d'entrer manuellement les valeurs de la matrice.
+     *
+     * @param matrix La matrice à remplir avec les valeurs saisies par l'utilisateur.
+     * @param scanner L'objet Scanner pour prendre les entrées de l'utilisateur.
+     */
     private static void enterMatrixValues(int[][] matrix, Scanner scanner) {
         int size = matrix.length;
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                System.out.print("Entrez la rangée " + (i + 1) + ", colonne " + (j + 1) + ": ");
+                System.out.print("Entrez la rangée " + (i + 1) + ", colonne " + (j + 1) + " : ");
                 matrix[i][j] = scanner.nextInt();
             }
         }
     }
 
+    /**
+     * Remplit la matrice avec une valeur automatique spécifiée.
+     *
+     * @param matrix La matrice à remplir avec la valeur automatique.
+     * @param scanner L'objet Scanner pour prendre l'entrée de l'utilisateur pour la valeur automatique.
+     */
     private static void autoMatrixValues(int[][] matrix, Scanner scanner) {
         int size = matrix.length;
         System.out.print("Valeur 'auto' : ");
@@ -73,6 +102,11 @@ public class Main {
         }
     }
 
+    /**
+     * Imprime la matrice donnée.
+     *
+     * @param matrix La matrice à imprimer.
+     */
     private static void printMatrix(int[][] matrix) {
         for (int[] row : matrix) {
             for (int value : row) {
@@ -80,14 +114,20 @@ public class Main {
             }
             System.out.println();
         }
+        System.out.println();
     }
 
+    /**
+     * Remplit la matrice avec des valeurs aléatoires.
+     *
+     * @param matrix La matrice à remplir avec des valeurs aléatoires.
+     */
     private static void randomMatrix(int[][] matrix) {
         int size = matrix.length;
         System.out.print("Matrice 'random' : " + '\n');
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                matrix[i][j] = (int)(Math.random() * 50);
+                matrix[i][j] = (int) (Math.random() * 50);
             }
         }
     }
--- src/main/java/org/example/MatrixMultiplier.java
+++ src/main/java/org/example/MatrixMultiplier.java
@@ -2,7 +2,13 @@ package org.example;
 
 import java.util.concurrent.RecursiveTask;
 
+import java.util.concurrent.RecursiveTask;
+
+/**
+ * Classe représentant un multiplieur de matrices utilisant le framework Fork-Join en Java.
+ */
 public class MatrixMultiplier extends RecursiveTask<int[][]> {
+
     private final int[][] A; // Matrice A
     private final int[][] B; // Matrice B
     private final int[][] C; // Matrice résultante
@@ -11,7 +17,13 @@ public class MatrixMultiplier extends RecursiveTask<int[][]> {
     // Seuil pour la condition de base (à ajuster en fonction de la taille de la matrice)
     private static final int SEUIL = 8;
 
-    // Constructeur de la classe
+    /**
+     * Constructeur de la classe MatrixMultiplier.
+     *
+     * @param A    Matrice A à multiplier.
+     * @param B    Matrice B à multiplier.
+     * @param size Taille des matrices.
+     */
     public MatrixMultiplier(int[][] A, int[][] B, int size) {
         this.A = A;
         this.B = B;
@@ -19,11 +31,13 @@ public class MatrixMultiplier extends RecursiveTask<int[][]> {
         this.C = new int[size][size]; // Initialise la matrice résultante
     }
 
-    // Méthode principale pour effectuer la multiplication de matrices
+    /**
+     * Méthode principale pour effectuer la multiplication de matrices.
+     *
+     * @return Matrice résultante de la multiplication.
+     */
     @Override
     protected int[][] compute() {
-        printMatrix(A);
-        printMatrix(B);
 
         if (size < SEUIL) {
             // Condition de base : multiplication de matrices de petite taille
@@ -42,9 +56,7 @@ public class MatrixMultiplier extends RecursiveTask<int[][]> {
 
         // Divisez les matrices A et B en sous-matrices
         divideMatrix(A, A11, A12, A21, A22);
-
         divideMatrix(B, B11, B12, B21, B22);
-
 
         // Créez des tâches pour les multiplications des sous-matrices
         MatrixMultiplier m1 = new MatrixMultiplier(add(A11, A22), add(B11, B22), newSize);
@@ -68,7 +80,7 @@ public class MatrixMultiplier extends RecursiveTask<int[][]> {
         int[][] C11 = add(subtract(add(m1.join(), m4.join()), m5.join()), m7.join());
         int[][] C12 = add(m3.join(), m5.join());
         int[][] C21 = add(m2.join(), m4.join());
-        int[][] C22 = add(add(subtract(m1.join(), m4.join()), m3.join()), m6.join());
+        int[][] C22 = add(subtract(m1.join(), m2.join()), add(m3.join(), m6.join()));
 
         // Copiez les sous-matrices dans C
         copyMatrix(C, C11, C12, C21, C22);
@@ -77,7 +89,13 @@ public class MatrixMultiplier extends RecursiveTask<int[][]> {
         return C; // Renvoie la matrice résultante
     }
 
-    // Méthode pour multiplier deux matrices de base
+    /**
+     * Méthode pour multiplier deux matrices de base.
+     *
+     * @param A Matrice A.
+     * @param B Matrice B.
+     * @return Matrice résultante de la multiplication.
+     */
     private int[][] multiplyMatrices(int[][] A, int[][] B) {
         int size = A.length;
         int[][] result = new int[size][size];
@@ -91,7 +109,13 @@ public class MatrixMultiplier extends RecursiveTask<int[][]> {
         return result;
     }
 
-    // Méthode pour additionner deux matrices
+    /**
+     * Méthode pour additionner deux matrices.
+     *
+     * @param A Matrice A.
+     * @param B Matrice B.
+     * @return Matrice résultante de l'addition.
+     */
     private int[][] add(int[][] A, int[][] B) {
         int size = A.length;
         int[][] result = new int[size][size];
@@ -103,7 +127,13 @@ public class MatrixMultiplier extends RecursiveTask<int[][]> {
         return result;
     }
 
-    // Méthode pour soustraire deux matrices
+    /**
+     * Méthode pour soustraire deux matrices.
+     *
+     * @param A Matrice A.
+     * @param B Matrice B.
+     * @return Matrice résultante de la soustraction.
+     */
     private int[][] subtract(int[][] A, int[][] B) {
         int size = A.length;
         int[][] result = new int[size][size];
@@ -115,7 +145,15 @@ public class MatrixMultiplier extends RecursiveTask<int[][]> {
         return result;
     }
 
-    // Méthode pour diviser une matrice en 4 sous-matrices
+    /**
+     * Méthode pour diviser une matrice en 4 sous-matrices.
+     *
+     * @param matrix Matrice à diviser.
+     * @param C11    Sous-matrice en haut à gauche.
+     * @param C12    Sous-matrice en haut à droite.
+     * @param C21    Sous-matrice en bas à gauche.
+     * @param C22    Sous-matrice en bas à droite.
+     */
     private void divideMatrix(int[][] matrix, int[][] C11, int[][] C12, int[][] C21, int[][] C22) {
         int newSize = matrix.length / 2;
         for (int i = 0; i < newSize; i++) {
@@ -126,11 +164,17 @@ public class MatrixMultiplier extends RecursiveTask<int[][]> {
                 C22[i][j] = matrix[i + newSize][j + newSize];
             }
         }
-
-
     }
 
-    // Méthode pour copier les sous-matrices dans la matrice résultante
+    /**
+     * Méthode pour copier les sous-matrices dans la matrice résultante.
+     *
+     * @param result Matrice résultante.
+     * @param C11    Sous-matrice en haut à gauche.
+     * @param C12    Sous-matrice en haut à droite.
+     * @param C21    Sous-matrice en bas à gauche.
+     * @param C22    Sous-matrice en bas à droite.
+     */
     private void copyMatrix(int[][] result, int[][] C11, int[][] C12, int[][] C21, int[][] C22) {
         int newSize = result.length / 2;
         for (int i = 0; i < newSize; i++) {
@@ -140,15 +184,6 @@ public class MatrixMultiplier extends RecursiveTask<int[][]> {
                 result[i + newSize][j] = C21[i][j];
                 result[i + newSize][j + newSize] = C22[i][j];
             }
-        }
-    }
-
-    private static void printMatrix(int[][] matrix) {
-        for (int[] row : matrix) {
-            for (int value : row) {
-                System.out.print(value + " ");
-            }
-            System.out.println();
         }
     }
 }
